3be89dbf8d7a54325664d514b923bf70690855c3
mjc <mchowet@gmail.com>
2024-08-08 13:53:24 -0400

    Added logic for getRatio() Added price calculation in getDataPoint() In 'main' changed to store stock prices by name in dict Added printout of ratio between 'ABC' and 'DEF' Added time-from-begin to log to track timings in output

--- client3.py
+++ client3.py
@@ -20,6 +20,7 @@
 
 import json
 import random
+import time
 import urllib.request
 
 # Server API URLs
@@ -28,32 +29,40 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
+# Do we round prices?
+ROUND_PRICES = True
+
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    if ROUND_PRICES:
+        price = round((bid_price + ask_price) / 2, 2)
+    else:
+        price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return -1
+    return round(price_a / price_b, 8)
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
+    start_time = time.monotonic()
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            prices[stock] = price
+            print(f"[{time.monotonic()-start_time:10.3f}] Quoted {stock} at "
+                  f"(bid:{bid_price:.2f} | ask:{ask_price:.2f} | price: {price:.2f})")
 
-        print("Ratio %s" % getRatio(price, price))
+        print(f"[{time.monotonic()-start_time:10.3f}] Ratio {getRatio(prices["ABC"], prices["DEF"])}")
